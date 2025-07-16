f1b0405e404bc9c72f70b4181b9b3748ff77511e
Kim Beomhee <nakaldi@gmail.com>
2025-07-16 20:30:44 +0900

    feat: 인증 인가 예외를 공통된 ApiResponse 형식으로 반환하도록 함

--- src/main/java/io/cavia/trader/common/config/SecurityConfig.java
+++ src/main/java/io/cavia/trader/common/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package io.cavia.trader.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cavia.trader.common.exception.ErrorCode;
+import io.cavia.trader.common.response.ApiResponse;
 import io.cavia.trader.module.jwt.JwtAuthenticationFilter;
 import io.cavia.trader.module.jwt.JwtUtil;
 import io.cavia.trader.module.member.repository.MemberRepository;
@@ -8,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +28,7 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final MemberRepository memberRepository;
+    private final ObjectMapper objectMapper;
 
     // PasswordEncoder를 Bean으로 등록
     @Bean
@@ -60,18 +66,33 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
         );
 
-        http.exceptionHandling(exception -> exception
-                // 1. 인증되지 않은 사용자가 보호된 리소스에 접근할 때 호출될 핸들러를 설정합니다.
-                .authenticationEntryPoint((request, response, authException) -> {
-                    // 클라이언트에게 401 Unauthorized 에러를 응답합니다.
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요합니다.");
-                })
-                // 2. 인증은 되었지만, 특정 리소스에 접근할 권한이 없을 때 호출될 핸들러를 설정합니다.
-                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                    // 클라이언트에게 403 Forbidden 에러를 응답합니다.
-                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
-                })
-        );
+
+        // 인증되지 않은 사용자가 보호된 리소스에 접근할 때 호출될 핸들러를 설정합니다.
+        http.exceptionHandling(exception -> {
+            // 인증 실패 시 처리
+            exception.authenticationEntryPoint((request, response, authException) -> {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding("UTF-8");
+
+                // 공통된 응답을 위해 직접 만든 리스폰스 DTO를 사용
+                ApiResponse<?> apiResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "미인증된 사용자입니다", null);
+
+                // ObjectMapper를 사용해 JSON 으로 변환하여 응답
+                objectMapper.writeValue(response.getWriter(), apiResponse);
+            });
+
+            // 인가 실패 시 처리
+            exception.accessDeniedHandler((request, response, accessDeniedException) -> {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding("UTF-8");
+
+                ApiResponse<?> apiResponse = new ApiResponse<>(HttpStatus.FORBIDDEN.value(), "접근 권한이 없습니다", null);
+
+                objectMapper.writeValue(response.getWriter(), apiResponse);
+            });
+        });
 
         // 커스텀 필터인 JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
